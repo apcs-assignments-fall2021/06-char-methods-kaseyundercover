313d752c1e716b802acb0719912f227b0e7a1afd
kaseyk9 <23kkazliner@athenian.org>
2021-09-16 23:25:09 -0700

    I did fine on the first 2 methods, as for the third method, i kept getting stuck. I spent over 2 hours trying different ways but I couldn't get the desired result.

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,28 +4,51 @@ public class MyMain {
     // If the char is not an uppercase letter, 
     // it is returned unchanged. 
     public static char toLower(char ch) {
-        // REPLACE WITH YOUR CODE
-        return '!';
+        int minlower = 97;
+        if (ch < minlower && ch>64) {
+             char x =(char)(ch + 32);
+            return x;
+        } else {
+            return ch;
+        }
     }
 
     // Changes a lowercase letter to an uppercase letter
     // If the char is not a lowercase letter,
     // it is returned unchanged.
     public static char toUpper(char ch) {
-        // REPLACE WITH YOUR CODE HERE
-        return '!';
-    }
+        int minlower=97;
+
+
+        if (ch>=minlower){
+         char y=(char)(ch-32);
+        return y;}
+
+        else if (ch>=65){
+            return ch;}
+
+
+        return ch;}
+
 
     // The method is given a String as input and returns a
     // new String where all of the numbers have been removed
     // from the original String.
     public static String removeNumbers(String str) {
-        // REPLACE WITH YOUR CODE
-        return "!!!";
-    }
+String result = " ";
+        for (char i=58; i<str.length(); i ++) {
+        if (i > 58){
+            result=result+i;
+            }
+        }
 
+    return result;
+    }
     // Write some code to test your methods!
     public static void main(String[] args) {
-        // ADD SOME TESTING CODE HERE
+        System.out.println(toLower('B'));
+        System.out.println(toUpper('B'));
+        System.out.println(removeNumbers("I have 2 shoes11111"));
+
     }
 }
